ee121b77dca2c5625c15855afb08929a9d497e3c
Dennis <theododimo@gmail.com>
2026-07-15 11:43:51 +0300

    Finish

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function( string, num ) {
+    let reply = "";
+    if ( num < 0 )
+    {
+        return 'ERROR';
+    }
+    for(let i = 0; i < num; i++)
+    {
+        reply += string;
+    }
+    return reply;
 };
 
 // Do not edit below this line
